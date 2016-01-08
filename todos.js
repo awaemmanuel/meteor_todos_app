1928a9429362c68bbecd18b981c3d7dddde7688c
Emmanuel Awa <eawa@brandeis.edu>
2016-01-08 03:39:51 -0500

    Removed package insecure, and then added Meteor.methods to give access to only signed in users. We added user accounts that is now used for authentication

--- todos.js
+++ todos.js
@@ -13,10 +13,7 @@ if (Meteor.isClient) {
         "submit .new-todo": function(event) {
             var text = event.target.todoText.value;
             
-            Todos.insert({
-                text: text,
-                createdAt: new Date()
-            });
+           Meteor.call('addTodo', text);
             
             // Clear form
             event.target.todoText.value = '';
@@ -26,13 +23,38 @@ if (Meteor.isClient) {
         },
         
         "click .toggle-checked": function() {
-            Todos.update(this._id, {$set: {checked: ! this.checked }});
+            Meteor.call('setChecked', this._id, !this.checked);
         },
         
         "click .delete-todo": function() {
             if(confirm('Are you sure?')) {
-                Todos.remove(this._id);
+                Meteor.call('deleteTodo', this._id);
             }
         }
     });
+    
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    })
 }
+
+// Meteor Methods
+Meteor.methods({
+    addTodo: function(text){
+        if(!Meteor.userId()){
+            throw new Meteor.Error('Not Authorized!');
+        }
+        Todos.insert({
+            text: text,
+            createdAt: new Date(),
+            userId: Meteor.userId(),
+            username: Meteor.user().username
+        });  
+    },
+    deleteTodo: function(todoId){
+        Todos.remove(todoId);
+    },
+    setChecked: function(todoId, setChecked){
+        Todos.update(todoId, {$set: {checked: setChecked}});
+    }
+});
